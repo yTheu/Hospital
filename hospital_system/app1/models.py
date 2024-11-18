2c162b0bce76e253cad15e76ec4d098c8862e45e
Matheus <matheuscs0v7@gmail.com>
2024-11-18 09:08:55 -0300

    implementação da classe Situacao

--- hospital_system/app1/models.py
+++ hospital_system/app1/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Paciente(models.Model):
+    class Situacao(models.TextChoices):
+        LEVE = 'leve', 'leve'
+        ESTAVEL = 'estavel', 'estavel'
+        GRAVE = 'grave', 'grave'
+
     nome = models.CharField(max_length=100, blank=False)
     sintoma = models.CharField(max_length=10, blank=False)
-    #situacao = models.Choices ["leve": "Leve","estavel": "Estável","grave": "Grave"]
+    situacao = models.CharField(max_length=10, choices=Situacao.choices, default=Situacao.leve, blank=False)
     cpf = models.CharField(max_length=11, blank=False)
     data_nascimento = models.DateField(blank=False)
     endereco = models.CharField(max_length=250, blank=False)
@@ -16,7 +21,7 @@ class Paciente(models.Model):
 class Medico(models.Model):
     nome = models.CharField(max_length=100, blank=False)
     cpf = models.CharField(max_length=11, blank=False)
-    especializacao = models.CharField(max_lenght=100, blank=100)
+    especializacao = models.CharField(max_length=100, blank=100)
     disponibilidade = models.BooleanField(bool)
     data_nascimento = models.DateField(blank=False)
     endereco = models.CharField(max_length=250, blank=False)
